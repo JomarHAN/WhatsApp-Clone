019598614e60cf50c60487ca90ebf994fbeaebd1
JOMAR HONG-AN NGUYEN <jomarhan.ng@gmail.com>
2020-10-28 15:40:23 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyATd5_6bN0af1eHpfayI7Gy1Ras9VDkOEk",
+    apiKey: "FIREBASE_APIKEY",
     authDomain: "whatsapp-clone-78a4c.firebaseapp.com",
     databaseURL: "https://whatsapp-clone-78a4c.firebaseio.com",
     projectId: "whatsapp-clone-78a4c",
     storageBucket: "whatsapp-clone-78a4c.appspot.com",
-    messagingSenderId: "1018284716630",
-    appId: "1:1018284716630:web:ce71a51ab216c7cf5941f7",
-    measurementId: "G-3GP5ENZ790"
+    messagingSenderId: "FIREBASE_MESSAGE_SENDER-ID",
+    appId: "FIREBASE_APPID",
+    measurementId: "FIREBASE_MEASURENMENT_ID"
 })
 
 const db = firebaseApp.firestore();
